3b7db800d0a0fd3ed11bb9cf1f21bef27b7a31b6
anierbeck <bcanhome@googlemail.com>
2014-02-04 21:52:00 +0100

    added recipe 6 for servlet-only WAB.

---
+++ chapter5/chapter5-recipe6/src/main/java/com/packt/HelloServlet.java
@@ -0,0 +1,34 @@
+package com.packt;
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet (value="/test", name="test")
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet Get</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
