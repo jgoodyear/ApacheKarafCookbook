8f221e98dfd0368e43f9096997d4fca575d324e1
Heath Kesler <hkesler@savoirtech.com>
2014-03-10 20:26:58 -0600

    added recipe 3

---
+++ chapter5/chapter5-recipe3/src/main/java/com.example.test.cxf/StringRestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.test.cxf;
+
+/**
+ * Implementation of the Features REST service.
+ */
+public class StringRestServiceImpl implements StringRestService {
+    @Override
+    public String getRecipeThree() throws Exception {
+        System.out.println("RECIPE 3 :: restful endpoint hit.");
+        return "<packt><chapter5><recipe3>THIS IS A COOK BOOK RECIPE TWO.... BABY!!!</recipe3></chapter5></packt>";
+    }
+}
