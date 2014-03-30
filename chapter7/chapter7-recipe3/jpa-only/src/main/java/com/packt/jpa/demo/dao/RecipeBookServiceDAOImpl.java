77882d92cc6901539c956f3db9c597a0c464c53b
jgoodyear <jgoodyear@savoirtech.com>
2014-03-30 17:25:50 -0230

    Resource local, no container based tx

--- chapter7/chapter7-recipe3/jpa-only/src/main/java/com/packt/jpa/demo/dao/RecipeBookServiceDAOImpl.java
+++ chapter7/chapter7-recipe3/jpa-only/src/main/java/com/packt/jpa/demo/dao/RecipeBookServiceDAOImpl.java
@@ -7,34 +7,52 @@ import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.EntityExistsException;
 import javax.persistence.TransactionRequiredException;
+import javax.persistence.PersistenceUnit; 
+import javax.persistence.EntityManagerFactory; 
+import javax.persistence.EntityTransaction; 
+import javax.persistence.Persistence; 
 
 import java.lang.IllegalArgumentException;
 
 import com.packt.jpa.demo.entity.Recipe;
 import com.packt.jpa.demo.api.RecipeBookService;
 
+
 public class RecipeBookServiceDAOImpl implements RecipeBookService {
 
-    private EntityManager em;
+    @PersistenceUnit(unitName="recipe") 
+    private EntityManagerFactory factory;
 
-    public void setEntityManager(EntityManager em) {
-        this.em = em;
-    }   
+    public void setEntityManagerFactory(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
 
     @Override
     public List<Recipe> getRecipes() {
         List<Recipe> result = new ArrayList<Recipe>();
-        result = em.createQuery("select r from RECIPE r", Recipe.class).getResultList();
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        result = entityManager.createQuery("select r from RECIPE r", Recipe.class).getResultList();
+        entityTransaction.commit();
         return result;
     }
 
     @Override
     public void addRecipe(String title, String ingredients) {
-        em.persist(new Recipe(title, ingredients));
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.persist(new Recipe(title, ingredients));
+        entityTransaction.commit();
     }
 
     @Override
     public void deleteRecipe(String title) {
-        em.remove(em.getReference(Recipe.class, title));
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.remove(entityManager.getReference(Recipe.class, title));
+        entityTransaction.commit();
     }
 }
